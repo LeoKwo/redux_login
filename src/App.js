726eeb301c9c9c8e1dca9d57fd3d04c6cc402a13
LeoKwo <leokwok1997@hotmail.com>
2022-06-29 21:56:10 -0400

    redux login mockup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import {useSelector, useDispatch} from 'react-redux';
-import {increment, decrement} from './actions'
+import Login from './components/login.js'
+import Logout from './components/logout.js'
+import { selectUser } from './features/userSlice';
+
 
 function App() {
-  const counter = useSelector(state => {
-    return state.counter
-  })
-  const isLogged = useSelector(state => {
-    return state.isLogged
-  })
-  const dispatch = useDispatch()
 
+  const user = useSelector(selectUser);
   return (
     <div className="App">
       <h1>Redux</h1>
-      <p>Counter {counter}</p>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(increment(5))}>+5</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-
-      {isLogged && <p>This should not be seen if not logged in</p>}
+      <div>{
+        user ? <Logout /> : <Login />
+      }</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers } from 'redux';
-import allReducer from './reducers/index.js'
 import { Provider } from 'react-redux'; // connects our global states, the store the our entire app
-
-const myStore = createStore(
-  allReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // this is for chrome extension
-)
+import store from "./app/store";
 
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <Provider store={myStore}>
+  <Provider store={store}>
     <App tab="home" />
   </Provider>
 );
